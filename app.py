e75b9bba0ef33d55c93bf11e4cf1d0d99092d1ff
Dracgamer5643 <supermee5643@gmail.com>
2024-10-05 19:21:45 +0530

    Added annual Report page in the section of userpage

--- app.py
+++ app.py
@@ -69,10 +69,6 @@ def login():
 def signup():
     return render_template('Register.html')
 
-@app.route("/annualReport")
-def annualReport():
-    return render_template('anualReport.html')
-
 @app.route('/video_feed')
 def video_feed():
     global camera_active
